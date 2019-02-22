a939a4fc17a2b32208454e4c822c01b3aa6afb99
ThibautDemare <fdhp_76@hotmail.com>
2019-02-22 09:30:16 +0100

    Redirect the output of the docker run into a file for debug/log purpose

--- execDockers.py
+++ execDockers.py
@@ -24,7 +24,7 @@ with open('params.csv') as csv_file:
 			while nbSimPerParam < maxNbSimPerParam:
 				resultPath = "Result_"+str(nbSimPerParam)+"_"+row[1]+"_"+row[2]+"_"+row[3]+"_"+row[4]
 				Popen("mkdir Results/"+resultPath, shell = True)
-				command = ["docker run --name scilab"+str(simId)+" --rm  -v $(pwd)/scilab-scripts:/scilab-scripts -v $(pwd)/Results/"+resultPath+":/Result scilab "+scilabScriptName+" "+row[1]+" "+row[2]+" "+row[3]+" "+row[4]]
+				command = ["docker run --name scilab"+str(simId)+" --rm  -v $(pwd)/scilab-scripts:/scilab-scripts -v $(pwd)/Results/"+resultPath+":/Result scilab "+scilabScriptName+" "+row[1]+" "+row[2]+" "+row[3]+" "+row[4] + " > output_docker_"+str(simId)]
 				Popen(command, shell = True)
 				simId += 1
 				nbSimPerParam += 1
