317cf206e17c8f4daeb93d8ceec089229b1f1ba4
ThibautDemare <fdhp_76@hotmail.com>
2019-03-01 16:50:16 +0100

    Add the index in matrix of parameters in the folder name

--- execDockers.py
+++ execDockers.py
@@ -22,7 +22,7 @@ with open('params.csv') as csv_file:
 		else:
 			nbSimPerParam = 0
 			while nbSimPerParam < maxNbSimPerParam:
-				resultPath = "Result_"+str(nbSimPerParam)+"_"+row[1]+"_"+row[2]+"_"+row[3]+"_"+row[4]
+				resultPath = "Result_"+row[0]+"_"+str(nbSimPerParam)+"_"+row[1]+"_"+row[2]+"_"+row[3]+"_"+row[4]
 				run("mkdir Results/"+resultPath, shell = True)
 				command = ["docker run --name scilab"+str(simId)+" --rm  -v $(pwd)/scilab-scripts:/scilab-scripts -v $(pwd)/Results/"+resultPath+":/Result scilab "+scilabScriptName+" "+row[1]+" "+row[2]+" "+row[3]+" "+row[4] + " > output_docker_"+str(simId)]
 				Popen(command, shell = True)
