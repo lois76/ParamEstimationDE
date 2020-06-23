3900a7e4d3cec75bbdcfe7f32e9755aa78944d82
Loïs Naudin <lois.naudin@gmail.com>
2020-06-23 12:50:38 +0200

    ...

--- differentialEvolution/entrypoint.sh
+++ differentialEvolution/entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-scilab-adv-cli -f main.sce -nb -args $1 $2 $3 $4 $5 #first arg is the name of script containing the simulation, other args are the parameters
+
+# $2=taillePop, $3=nbiter, $4=F, $5=CR
+
+scilab-adv-cli -f main.sce -nb -args $1 $2 $3 #first arg is the name of script containing the simulation, other args are the parameters 
+
+
